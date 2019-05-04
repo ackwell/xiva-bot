95333d507816bcad7d9b3b03c06f4f7a44e6f57e
Saxon Landers <saxon@ackwell.com.au>
2019-05-05 00:08:44 +1000

    Slightly more advaced command parsing

--- lib/bot/commands.ex
+++ lib/bot/commands.ex
@@ -1,7 +1,42 @@
 defmodule Bot.Commands do
   use Coxir.Commander
+  # import Coxir.Commander.Utils
 
-  @prefix "|>"
+  @prefix "|> "
 
-  command ping, do: Message.reply(message, "pong")
+  # @permit &is_admin?/2 - broken on current rev
+  @permit :ADMINISTRATOR
+  command set_reaction(arguments) do
+    [emoji, role | _rest] = String.split(arguments)
+
+    case role_id(role) do
+      {:incorrect} -> respond_fail(message)
+      {:ok, role_id} ->
+        emoji_id = case custom_emoji_id(emoji) do
+          {:incorrect} -> emoji
+          {:ok, id} -> id
+        end
+
+        respond_success(message)
+    end
+  end
+
+  defp respond_success(message) do
+    Message.react(message, "✅")
+  end
+
+  defp respond_fail(message) do
+    Message.react(message, "❌")
+  end
+
+  defp role_id(format) do
+    first_group(Regex.run(~r/<@&(\d+)>/, format))
+  end
+
+  defp custom_emoji_id(format) do
+    first_group(Regex.run(~r/<a?:.+:(\d+)>/, format))
+  end
+
+  defp first_group(nil), do: {:incorrect}
+  defp first_group([_match, group]), do: {:ok, group}
 end
