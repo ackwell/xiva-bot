d927a89e3ca3b6f05ae3309efc6e9225eb2046df
Saxon Landers <saxon@ackwell.com.au>
2019-05-05 11:36:01 +1000

    Add remove role handling

--- lib/bot/consumer.ex
+++ lib/bot/consumer.ex
@@ -2,11 +2,8 @@ defmodule Bot.Consumer do
   use Coxir
 
   # :MESSAGE_REACTION_REMOVE
-  @impl true
   def handle_event({:MESSAGE_REACTION_ADD, reaction}, state) do
-    emoji = reaction.emoji.id || reaction.emoji.name
-    role = Bot.Config.ReactionRole.get_role(reaction.message_id, emoji)
-
+    role = get_reaction_role(reaction)
     if role do
       member = Member.get(reaction.guild_id, reaction.user_id)
       Member.add_role(member, role)
@@ -15,8 +12,23 @@ defmodule Bot.Consumer do
     {:ok, state}
   end
 
-  @impl true
+  def handle_event({:MESSAGE_REACTION_REMOVE, reaction}, state) do
+    role = get_reaction_role(reaction)
+    if role do
+      member = Member.get(reaction.guild_id, reaction.user_id)
+      Member.remove_role(member, role)
+    end
+
+    {:ok, state}
+  end
+
+  # Fall back to commander handler
   def handle_event(event, state) do
     Bot.Commands.handle_event(event, state)
+  end
+
+  defp get_reaction_role(reaction) do
+    emoji = reaction.emoji.id || reaction.emoji.name
+    Bot.Config.ReactionRole.get_role(reaction.message_id, emoji)
   end
 end
